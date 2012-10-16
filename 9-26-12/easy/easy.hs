5bd34b95dfdc5b05367f8a4537d1d251c4fb8d35
Thomas Crescenzi <trescenzi@gmail.com>
2012-10-16 20:18:45 +0900

    added my solutions to the first hacknight that I have solutions for

---
+++ 9-26-12/easy/easy.hs
@@ -0,0 +1,3 @@
+--Soultion to palindrome. I commented out the type defination just to show how it's not actually necessary
+--pali :: String -> Bool
+pali string = reverse string == string
---
+++ 9-26-12/medium/medium.hs
@@ -0,0 +1,13 @@
+--My solution to the sexy primes. The prime generation is actually kinda slow but in this case I doubt it matters
+primes :: [Int]
+primes = sieve [2..]
+  where
+    sieve (p:xs) = p : sieve [x|x <- xs, x `mod` p > 0] 
+
+--check to see if a number is contained in the set of primes
+isPrime :: Int -> Bool
+isPrime x = elem x (take x primes)
+
+--a function that passes a limit into a list comprehension which generates
+--infinite sexy primes
+sexyPrimes n = [ (x, x+6) | x <- [3..n-6], isPrime x, isPrime (x+6)] 
