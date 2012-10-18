def360d98307240573ea3fb78fbe5e8807c27024
Dan Miller <dan@danielsmiller.com>
2012-10-17 23:00:26 -0300

    Update 9-26-12/easy/easy.hs

--- 9-26-12/easy/easy.hs
+++ 9-26-12/easy/easy.hs
@@ -1,3 +1,4 @@
 --Soultion to palindrome. I commented out the type defination just to show how it's not actually necessary
+--Hi Thom!!!
 --pali :: String -> Bool
 pali string = reverse string == string
