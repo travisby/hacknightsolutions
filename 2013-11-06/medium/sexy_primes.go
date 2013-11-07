cd934bad95bc293d4a3ad42d13ede3956890d772
Travis Beatty <travisby@gmail.com>
2013-11-06 22:17:16 -0500

    Added simple iterative implementation

--- 2013-11-06/medium/sexy_primes.go
+++ 2013-11-06/medium/sexy_primes.go
@@ -4,16 +4,39 @@ import "os"
 import "strconv"
 import "fmt"
 
+func isPrime(n int) bool {
+	// definition
+	if n == 1 {
+		return false
+	}
+
+	for i := n - 1; i > 1; i-- {
+		if n%i == 0 {
+			return false
+		}
+	}
+	return true
+}
+
 func listSexy(n int) [][]int {
-	fmt.Print(n)
-	return [][]int{}
+	var results [][]int
+
+	for i := 0; i <= n; i++ {
+		m := i + 6
+		if isPrime(m) && isPrime(i) {
+			results = append(results, []int{i, m})
+		}
+	}
+
+	return results
 }
 
 func main() {
-	var n int64
+	var n int
 	var _ error
 
-	n, _ = strconv.ParseInt(os.Args[0], 10, 0)
+	n, _ = strconv.Atoi(os.Args[1])
 
-	fmt.Print(listSexy(int(n)))
+	fmt.Print(listSexy(n))
+	fmt.Print("\n")
 }
