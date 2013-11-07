82f773eed3523ef0f2fb6c1ce09fa55116a3aa08
Travis Beatty <travisby@gmail.com>
2013-11-06 21:24:47 -0500

    Ran gofmt... woops

--- 2013-11-06/easy/palindrome.go
+++ 2013-11-06/easy/palindrome.go
@@ -3,18 +3,18 @@ package main
 import "fmt"
 
 func isPalindrome(str string) bool {
-    for i:= len(str) - 1; i >= 0; i-- {
-        if string(str[i]) != string(str[(len(str) - 1) - i]) {
-            return false
-        }
-    }
-    return true
+	for i := len(str) - 1; i >= 0; i-- {
+		if string(str[i]) != string(str[(len(str)-1)-i]) {
+			return false
+		}
+	}
+	return true
 }
 
 func main() {
-    var input string
-    fmt.Print("Please enter a string for me to determine its palindromeosity: ")
-    fmt.Scanf("%s", &input)
-    fmt.Print(isPalindrome(input))
-    fmt.Print("\n")
+	var input string
+	fmt.Print("Please enter a string for me to determine its palindromeosity: ")
+	fmt.Scanf("%s", &input)
+	fmt.Print(isPalindrome(input))
+	fmt.Print("\n")
 }
